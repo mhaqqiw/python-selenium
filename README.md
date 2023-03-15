6fea1f469c3bf60fea785fc51616d1911ac1ce52
haqqi.wicaksono <haqqi.wicaksono@tokopedia.com>
2023-03-15 22:13:02 +0700

    no message

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # python-selenium
+
+## Test Scenario
+https://www.online-calculator.com/full-screen-calculator/
+
+## Folder Structure
+```
+.
+├── main.py
+├── test_listener.py
+    └── before suite
+    └── after suite
+    └── before test
+    └── after test
+└── elements
+└── internal
+└── testsuites
+└── testcases
+└── reports
+```
+
+## Prerequisite
+- Python3
+- Pip3
+
+## Run
+- `python3 -m pip install -r requirements.txt`
+- `python3 main.py`
+
+## Parameter
+| Key  | Value  | Default  |  Mandatory |   |
+|---|---|---|---|---|
+|  --test-suite-path |  path |  testsuites |  :x: |   |
+
+## Next Feature
+- Decorator
+    - This feature will added flag to testcase & test listener function
+- Parallel
+    - Will add parameter to run testcases parallel with configurable worker
+- HTML
+    - Will generate Report in HTML
+- Browser Selection
+    - Will add another browser and can run headless
+- Dockerization
+    - Will Dockerfile to make easier to run with Docker
--- main.py
+++ main.py
@@ -6,35 +6,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--test-suite-path", help="This use if you have spesific Test Suite to be tested", default="testsuites")
 args = parser.parse_args()
 
-# canvas = '//div[@id="animation_container"]'
-# iframe_canvas = '//iframe[@id="fullframe"]'
-
-# driver = webdriver.Chrome()
-# driver.get("https://www.online-calculator.com/full-screen-calculator/")
-# # driver.fullscreen_window()
-# driver.switch_to.frame(driver.find_element(By.XPATH, iframe_canvas))
-
-# elem = driver.find_element(By.XPATH, canvas)
-# elem.click()
-# elem_dimension = elem.location
-# size = elem.size
-# w, h = size['width'], size['height']
-
-# left = elem_dimension['x']
-# top = elem_dimension['y']
-# right = left + w
-# bottom = top + 125
-# coor = {"x": 75.5, "y": 331}
-# print(coor)
-
-# module.clickCoordinate(driver, elem, coor)
-# time.sleep(100)
-# calc.run_click(driver, "1+1=2")
-
-# module.get_text_from_canvas(driver, left, top, right, bottom)
-
-# driver.close()
-
 def get_total(list_file):
     for i in list_file:
         data = json.load(open(i, 'r'))
